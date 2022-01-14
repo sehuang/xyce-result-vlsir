3bc305a2eb689b5140620df6733bcab6b018deb8
Sean Huang <sehuang@berkeley.edu>
2022-01-14 11:11:34 -0800

    Committing some changes

--- src/parse_result.py
+++ src/parse_result.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 import csv
 from abc import ABC
-from typing import Mapping, Any, Union
+from typing import Mapping, Any, Union, List
         
         
 class ResultFile(ABC):
     def __init__(self, file: str) -> None:
         self.file = file
     
-    def accept(self, visitor: FileVisitor) -> Mapping[Any, Any]:
+    def accept(self, visitor: FileVisitor) -> List[float]:
         pass
 
 
@@ -17,7 +17,7 @@ class CSVFile(ResultFile):
     def __init__(self, file: str) -> None:
         super().__init__(file)
         
-    def accept(self, visitor: FileVisitor) -> Mapping[Any, Any]:
+    def accept(self, visitor: FileVisitor) -> List[float]:
         return visitor.visit_csv(self)
     
 
@@ -25,39 +25,33 @@ class PRNFile(ResultFile):
     def __init__(self, file: str) -> None:
         super().__init__(file)
         
-    def accept(self, visitor: FileVisitor) -> Mapping[Any, Any]:
+    def accept(self, visitor: FileVisitor) -> List[float]:
         return visitor.visit_prn(self)
     
     
 class FileVisitor:
-    def visit_csv(self, component: ResultFile) -> Mapping[Any, Any]:
+    def visit_csv(self, component: ResultFile) -> List[float]:
         # Parse a CSV output file
         with open(component.file, 'r') as csvfile:
             reader = csv.reader(csvfile)
             vectors = reader.__next__()
-            results = dict(zip(vectors, [[]] * len(vectors)))
-            idx = enumerate(vectors)
+            results = []
             for row in reader:
-                for index in idx:
-                    results[index[1]].append(float(row[index[0]]))
+                results.append(row)
         return results
             
-    def visit_prn(self, component: ResultFile) -> Mapping[Any, Any]:
+    def visit_prn(self, component: ResultFile) -> List[float]:
         # Parse default text-based output file
         with open(component.file, 'r') as prnfile:
             lines = prnfile.readlines()
             vectors = lines[0].split()
-            results = dict(zip(vectors, [[]] * len(vectors)))
-            idx = enumerate(vectors)
-            for row in lines:
-                for index in idx:
-                    results[index[1]].append(float(row[index[0]]))
-            # If .PRINT given FORMAT=NOINDEX option, there will not be an index 
-            # column. If this column is present, drop it as it is redundant
-            if "Index" in vectors: results.pop("Index")
+            results = []
+            drop_index = "Index" in vectors
+            for line in lines:
+                results.append([float(pt) for pt in line.split()[int(drop_index):]])
             return results
         
-    def visit_csd(self, component: ResultFile) -> Mapping[Any, Any]:
+    def visit_csd(self, component: ResultFile) -> List[float]:
         # Parse SPICE Probe file
         with open(component.file, 'r') as csdfile:
             lines = csdfile.readlines()
@@ -68,17 +62,15 @@ class FileVisitor:
             data_start_idx = lines.index("#C")
             # vectors = " ".join(lines[vector_start_idx:data_start_idx]).split()
             vectors = lines[vector_start_idx:data_start_idx]
-            results = dict(zip(['Time'] + vectors, [[]] * len(vectors)))
-            index = enumerate(vectors)
+            results = []
             # CSD Files also appear to end with a "#;" ending line, trim this
             for line in lines[data_start_idx:-1]:
                 if "#C" in line:
                     # If it's a time line
-                    results['Time'].append(line.split()[1])
+                    results.append(float(line.split()[1]))
                 else:
                     # Every data point is given as <data>:<column #>, trim off 
                     # the column numbers
                     points = [float(x.split(":")[0]) for x in line.split()]
-                    for idx in index:
-                        results[idx[1]].append(points(idx[0]))
+                    results.append(points)
             return results
